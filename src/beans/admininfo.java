0ce2ed805748fe25c79b5e4d7fda2419e3e574f1
himanshu2998 <41501501+himanshu2998@users.noreply.github.com>
2019-10-03 23:51:08 +0530

    Add files via upload

---
+++ src/beans/admininfo.java
@@ -0,0 +1,30 @@
+package beans;
+
+public class admininfo {
+	String language,name,radio;
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRadio() {
+		return radio;
+	}
+
+	public void setRadio(String radio) {
+		this.radio = radio;
+	}
+	
+}
